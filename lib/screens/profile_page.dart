deed061abc89c72cf68d2ab94034fe24f9af3823
mankirat <mankirat.singh158@gmail.com>
2020-10-26 01:56:32 +0530

    done

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -19,86 +19,97 @@ class _Profile_PageState extends State<Profile_Page> {
     return Scaffold(
       appBar: AppBar(
         title: Text('profile'),
+        backgroundColor: Colors.teal,
       ),
       backgroundColor: Colors.teal,
-      body: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: TextFormField(
-                  controller: nameController,
-                  decoration: InputDecoration(
-                    labelText: "Enter Name",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Container(
+          color: Colors.tealAccent,
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 50,
+                    width: 50,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: TextFormField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        labelText: "Enter Name",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      // The validator receives the text that the user has entered.
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Enter Name';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Enter Name';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: TextFormField(
-                  controller: QualificationController,
-                  decoration: InputDecoration(
-                    labelText: "Enter Qualification",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: TextFormField(
+                      controller: QualificationController,
+                      decoration: InputDecoration(
+                        labelText: "Enter Qualification",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      // The validator receives the text that the user has entered.
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Enter Qualification';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Enter Qualification';
-                    }
-                    return null;
-                  },
-                ),
+                  Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          RaisedButton(
+                            color: Colors.lightBlue,
+                            onPressed: () {
+                              if (_formKey.currentState.validate()) {
+                                DBRef.push().set({
+                                  "name": nameController.text,
+                                  "Qualification": QualificationController.text,
+                                }).then((_) {
+                                  Scaffold.of(context).showSnackBar(SnackBar(
+                                      content: Text('Successfully Added')));
+                                  nameController.clear();
+                                  QualificationController.clear();
+                                }).catchError((onError) {
+                                  Scaffold.of(context).showSnackBar(
+                                      SnackBar(content: Text(onError)));
+                                });
+                              }
+                            },
+                            child: Text('Submit'),
+                          ),
+                          RaisedButton(
+                            color: Colors.amber,
+                            onPressed: () {
+                              Navigator.pushNamed(context, home_Page.id);
+                            },
+                            child: Text('Navigate'),
+                          ),
+                        ],
+                      )),
+                ],
               ),
-              Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      RaisedButton(
-                        color: Colors.lightBlue,
-                        onPressed: () {
-                          if (_formKey.currentState.validate()) {
-                            DBRef.push().set({
-                              "name": nameController.text,
-                              "Qualification": QualificationController.text,
-                            }).then((_) {
-                              Scaffold.of(context).showSnackBar(SnackBar(
-                                  content: Text('Successfully Added')));
-                              nameController.clear();
-                              QualificationController.clear();
-                            }).catchError((onError) {
-                              Scaffold.of(context).showSnackBar(
-                                  SnackBar(content: Text(onError)));
-                            });
-                          }
-                        },
-                        child: Text('Submit'),
-                      ),
-                      RaisedButton(
-                        color: Colors.amber,
-                        onPressed: () {
-                          Navigator.pushNamed(context, home_Page.id);
-                        },
-                        child: Text('Navigate'),
-                      ),
-                    ],
-                  )),
-            ],
+            ),
           ),
         ),
       ),
