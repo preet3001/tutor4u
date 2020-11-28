556f7ea1ab0834930a818660907d01a09602adbe
sirjan4665 <4665+sirjan@users.noreply.github.com>
2020-11-28 18:18:13 +0530

    changed alignment of buttons in home page tried to add swipe cards on home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tutor4u/screens/login_page.dart';
+import 'package:tutor4u/screens/profile_page.dart';
 import 'package:tutor4u/services/authentication.dart';
 import 'package:flutter/cupertino.dart';
-import 'MatchCard.dart';
+
 // ignore: camel_case_types
 class home_Page extends StatefulWidget {
   static const String id = 'home_page';
@@ -12,47 +14,38 @@ class home_Page extends StatefulWidget {
 // ignore: camel_case_types
 class _home_PageState extends State<home_Page> {
   final authentication auth = authentication();
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(   
-        title: Text(
-          'SUTA',
-          textAlign: TextAlign.center,
+        appBar: AppBar(
+          title: Text(
+            'SUTA',
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.white12,
         ),
-        backgroundColor: Colors.white12,
-      ),
-      backgroundColor: Colors.white70,
-      body:(
-    Child: Stack(
-      alignment: Alignment.center,
-          children: _getMatchCard(),
-      ),
-    );
-    }
-    void setState(Null Function() param0) {
-    }
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            RaisedButton(
-                child: Text('profile'),
-                onPressed: () {
-                  Navigator.pushNamed(context, Profile_Page.id);
-                }),
-            RaisedButton(
-                child: Text('SIGN OUT'),
-                onPressed: () async {
-                  await auth.signoutgoogle();
-                  Navigator.pushNamed(context, login_Page.id);
-                })
-          ],
-        ),
-      ),
+        backgroundColor: Colors.white70,
+        body: Center(
+          child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        RaisedButton(
+            child: Text('profile'),
+            onPressed: () {
+              Navigator.pushNamed(context, Profile_Page.id);
+            }),
+        RaisedButton(
+            child: Text('SIGN OUT'),
+            onPressed: () async {
+              await auth.signoutgoogle();
+              Navigator.pushNamed(context, login_Page.id);
+            })
+      ],
+    )
+    ,
+    )
+    ,
     );
   }
 }
